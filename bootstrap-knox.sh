66c9fee3d82be7132b3c39c6d5aab0aaddc6490a
Janos Matyas <janos.matyas@sequenceiq.com>
2014-04-07 13:58:19 +0200

    Update bootstrap-knox.sh

--- bootstrap-knox.sh
+++ bootstrap-knox.sh
@@ -13,8 +13,9 @@ $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
 
 # knox bootstrap 
-#java -jar bin/ldap.jar conf &
-#java -jar bin/gateway.jar
+
+java -jar bin/ldap.jar conf &>/tmp/knox/ldap.log &
+java -jar bin/gateway.jar &>/tmp/knox/gateway.log &
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
